32217223c04df8abc4b3a4293007749622e91bd9
rushizirpe <rushikesh.zirpe1234@gmail.com>
2024-09-06 23:00:29 +0530

    Added PDF Helper

---
+++ src/utils/pdf_helper.py
@@ -0,0 +1,10 @@
+# src/utils/pdf_utils.py
+
+import base64
+import streamlit as st
+
+def display_pdf(file):
+    with open(file, "rb") as f:
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="100%" height="600" type="application/pdf"></iframe>'
+    st.markdown(pdf_display, unsafe_allow_html=True)
